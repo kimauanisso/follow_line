a67085e9a478633f283af8b8d9f5249d9e1b53f1
Pedro Henrique Peres da Silva <phperesdasilva@gmail.com>
2019-08-23 14:10:39 -0300

    Teste Biblioteca do encoder

--- FollowArduinoVS_Bosch/src/main.cpp
+++ FollowArduinoVS_Bosch/src/main.cpp
@@ -1,19 +1,10 @@
 #include <Arduino.h>
 
-<<<<<<< HEAD
-void setup(){
-
-}
-
-void loop(){
-
-=======
 void setup() {
   //laranjajajaja
 }
 
 void loop() {
 
->>>>>>> ee351c0920e6fee14bb8c5e8713697038268db6a
 }
 
