040e0d6faa986ccbb43ce7e4542f75b78b962794
lucianopb <lpb7lpb@gmail.com>
2019-08-24 11:51:36 -0300

    adicionei pinup.h

---
+++ FollowArduinoVS_Bosch/lib/pinup.h
@@ -0,0 +1,31 @@
+//sensors
+#define STURN A0
+#define S1 A1
+#define S2 A2
+#define S3 A3
+#define S4 A5
+#define S5 A6
+#define S6 A4
+
+//left motor
+#define OUTB1 D0 //ENCODER
+#define OUTA1 D1 //ENCODER
+#define AIN1 D8  //MOTOR DIRECTION
+#define AIN2 D7  //MOTOR DIRECTION
+#define PWMA D6
+
+//right motor
+#define OUTB2 D9 //ENCODER
+#define OUTA2 D3 //ENCODER
+#define BIN1 D11 //MOTOR DIRECTION
+#define BIN2 D10 //MOTOR DIRECTION
+#define PWMB D12
+
+//peripheral
+#define BUTTON_PIN D13
+#define BUZZER_PIN D2
+#define BL_TX D4 //BLUETOOTH
+#define BL_RX D5 //BLUETOOTH
+
+//Battery
+//#define Vbat ADC_VBAT
