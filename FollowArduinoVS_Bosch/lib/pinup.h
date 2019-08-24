008214308e47e458bf0f6da058bd97a006ff2fe4
lucianopb <lpb7lpb@gmail.com>
2019-08-24 11:57:10 -0300

    mudei nomes de variaveis do pinup.h

--- FollowArduinoVS_Bosch/lib/pinup.h
+++ FollowArduinoVS_Bosch/lib/pinup.h
@@ -8,24 +8,24 @@
 #define S6 A4
 
 //left motor
-#define OUTB1 D0 //ENCODER
-#define OUTA1 D1 //ENCODER
-#define AIN1 D8  //MOTOR DIRECTION
-#define AIN2 D7  //MOTOR DIRECTION
-#define PWMA D6
+#define L_ENC_A D0  //ENCODER
+#define L_ENC_B D1  //ENCODER
+#define L_IN1 D8    //MOTOR DIRECTION
+#define L_IN2 D7    //MOTOR DIRECTION
+#define L_PWM D6    //MOTOR PWM
 
 //right motor
-#define OUTB2 D9 //ENCODER
-#define OUTA2 D3 //ENCODER
-#define BIN1 D11 //MOTOR DIRECTION
-#define BIN2 D10 //MOTOR DIRECTION
-#define PWMB D12
+#define R_ENC_A D9  //ENCODER
+#define R_ENC_B D3  //ENCODER
+#define R_IN1 D11   //MOTOR DIRECTION
+#define R_IN2 D10   //MOTOR DIRECTION
+#define R_PWM D12   //MOTOR PWM
 
 //peripheral
 #define BUTTON_PIN D13
 #define BUZZER_PIN D2
-#define BL_TX D4 //BLUETOOTH
-#define BL_RX D5 //BLUETOOTH
+#define BL_TX D4    //BLUETOOTH
+#define BL_RX D5    //BLUETOOTH
 
 //Battery
 //#define Vbat ADC_VBAT
